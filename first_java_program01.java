6d77863b3d03b27c19b83eb055f2fb123c6896fa
CodeWithSudip-07 <sudipkarmakar723@gmail.com>
2026-04-28 14:54:19 +0530

    First JAVA Program

---
+++ first_java_program01.java
@@ -0,0 +1,18 @@
+
+/* 
+
+// Latest JAVA Hello World
+void main(){
+    System.out.println("Hello World!");
+    System.out.println("7+3");
+    System.out.println(7+3);
+}
+
+*/
+
+// Classic Hello World
+public class first_java_program01 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
